bdd114e3805cc7932eabce0ca7d75373d8accf03
Ray Milkey <ray@onlab.us>
2015-02-03 18:05:39 -0800

    Remove IntentOperations class
    Change-Id: I72cb7cfa76a27b436d80cd08ab1b5f22d7dcac8e

--- apps/demo/src/main/java/org/onosproject/demo/DemoInstaller.java
+++ apps/demo/src/main/java/org/onosproject/demo/DemoInstaller.java
@@ -258,19 +258,9 @@ public class DemoInstaller implements DemoAPI {
          * @throws InterruptedException if the thread go interupted
          */
         private void trackIntents() throws InterruptedException {
-            int count = 0;
-            while (!latch.await(100, TimeUnit.NANOSECONDS)) {
-                if (intentBatchService.getPendingOperations().isEmpty()) {
-                    latch.countDown();
-                }
-                count++;
-                if (count > ITERATIONMAX) {
-                    log.warn("A batch is stuck processing. " +
-                                     "pending : {}",
-                             intentBatchService.getPendingOperations());
-                    shutdownAndAwaitTermination(randomWorker);
-                }
-            }
+            // TODO generate keys fr each set of intents to allow manager to throttle
+            // TODO may also look into the store to see how many operations are pending
+
             //if everything is good proceed.
             if (!randomWorker.isShutdown()) {
                 randomWorker.execute(this);
